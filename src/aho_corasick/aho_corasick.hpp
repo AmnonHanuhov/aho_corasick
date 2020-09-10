7ea176a7c99b4d9bfc44d1b8b9535d2c413af6ac
Amnon Hanuhov <ahanukov@redhat.com>
2020-09-10 23:20:03 +0300

    Added support for prefix and suffix matching
    Also got rid of redundant classes

--- src/aho_corasick/aho_corasick.hpp
+++ src/aho_corasick/aho_corasick.hpp
@@ -28,267 +28,14 @@
 #include <map>
 #include <memory>
 #include <set>
+#include <list>
 #include <string>
 #include <queue>
 #include <utility>
 #include <vector>
+#include <unordered_map>
 
 namespace aho_corasick {
-
-	// class interval
-	class interval {
-		size_t d_start;
-		size_t d_end;
-
-	public:
-		interval(size_t start, size_t end)
-			: d_start(start)
-			, d_end(end) {}
-
-		size_t get_start() const { return d_start; }
-		size_t get_end() const { return d_end; }
-		size_t size() const { return d_end - d_start + 1; }
-
-		bool overlaps_with(const interval& other) const {
-			return d_start <= other.d_end && d_end >= other.d_start;
-		}
-
-		bool overlaps_with(size_t point) const {
-			return d_start <= point && point <= d_end;
-		}
-
-		bool operator <(const interval& other) const {
-			return get_start() < other.get_start();
-		}
-
-		bool operator !=(const interval& other) const {
-			return get_start() != other.get_start() || get_end() != other.get_end();
-		}
-
-		bool operator ==(const interval& other) const {
-			return get_start() == other.get_start() && get_end() == other.get_end();
-		}
-	};
-
-	// class interval_tree
-	template<typename T>
-	class interval_tree {
-	public:
-		using interval_collection = std::vector<T>;
-
-	private:
-		// class node
-		class node {
-			enum direction {
-				LEFT, RIGHT
-			};
-			using node_ptr = std::unique_ptr<node>;
-
-			size_t              d_point;
-			node_ptr            d_left;
-			node_ptr            d_right;
-			interval_collection d_intervals;
-
-		public:
-			explicit node(const interval_collection& intervals)
-				: d_point(0)
-				, d_left(nullptr)
-				, d_right(nullptr)
-				, d_intervals()
-			{
-				d_point = determine_median(intervals);
-				interval_collection to_left, to_right;
-				for (const auto& i : intervals) {
-					if (i.get_end() < d_point) {
-						to_left.push_back(i);
-					} else if (i.get_start() > d_point) {
-						to_right.push_back(i);
-					} else {
-						d_intervals.push_back(i);
-					}
-				}
-				if (to_left.size() > 0) {
-					d_left.reset(new node(to_left));
-				}
-				if (to_right.size() > 0) {
-					d_right.reset(new node(to_right));
-				}
-			}
-
-			size_t determine_median(const interval_collection& intervals) const {
-				auto start = std::numeric_limits<size_t>::max();
-				auto end   = std::numeric_limits<size_t>::max();
-				for (const auto& i : intervals) {
-					auto cur_start = i.get_start();
-					auto cur_end = i.get_end();
-					if (start == std::numeric_limits<size_t>::max() || cur_start < start) {
-						start = cur_start;
-					}
-					if (end == std::numeric_limits<size_t>::max() || cur_end > end) {
-						end = cur_end;
-					}
-				}
-				return (start + end) / 2;
-			}
-
-			interval_collection find_overlaps(const T& i) {
-				interval_collection overlaps;
-				if (d_point < i.get_start()) {
-					add_to_overlaps(i, overlaps, find_overlapping_ranges(d_right, i));
-					add_to_overlaps(i, overlaps, check_right_overlaps(i));
-				} else if (d_point > i.get_end()) {
-					add_to_overlaps(i, overlaps, find_overlapping_ranges(d_left, i));
-					add_to_overlaps(i, overlaps, check_left_overlaps(i));
-				} else {
-					add_to_overlaps(i, overlaps, d_intervals);
-					add_to_overlaps(i, overlaps, find_overlapping_ranges(d_left, i));
-					add_to_overlaps(i, overlaps, find_overlapping_ranges(d_right, i));
-				}
-				return interval_collection(overlaps);
-			}
-
-		protected:
-			void add_to_overlaps(const T& i, interval_collection& overlaps, interval_collection new_overlaps) const {
-				for (const auto& cur : new_overlaps) {
-					if (cur != i) {
-						overlaps.push_back(cur);
-					}
-				}
-			}
-
-			interval_collection check_left_overlaps(const T& i) const {
-				return interval_collection(check_overlaps(i, LEFT));
-			}
-
-			interval_collection check_right_overlaps(const T& i) const {
-				return interval_collection(check_overlaps(i, RIGHT));
-			}
-
-			interval_collection check_overlaps(const T& i, direction d) const {
-				interval_collection overlaps;
-				for (const auto& cur : d_intervals) {
-					switch (d) {
-					case LEFT:
-						if (cur.get_start() <= i.get_end()) {
-							overlaps.push_back(cur);
-						}
-						break;
-					case RIGHT:
-						if (cur.get_end() >= i.get_start()) {
-							overlaps.push_back(cur);
-						}
-						break;
-					}
-				}
-				return interval_collection(overlaps);
-			}
-
-			interval_collection find_overlapping_ranges(node_ptr& node, const T& i) const {
-				if (node) {
-					return interval_collection(node->find_overlaps(i));
-				}
-				return interval_collection();
-			}
-		};
-		node d_root;
-
-	public:
-		explicit interval_tree(const interval_collection& intervals)
-			: d_root(intervals) {}
-
-		interval_collection remove_overlaps(const interval_collection& intervals) {
-			interval_collection result(intervals.begin(), intervals.end());
-			std::sort(result.begin(), result.end(), [](const T& a, const T& b) -> bool {
-				if (b.size() - a.size() == 0) {
-					return a.get_start() > b.get_start();
-				}
-				return a.size() > b.size();
-			});
-			std::set<T> remove_tmp;
-			for (const auto& i : result) {
-				if (remove_tmp.find(i) != remove_tmp.end()) {
-					continue;
-				}
-				auto overlaps = find_overlaps(i);
-				for (const auto& overlap : overlaps) {
-					remove_tmp.insert(overlap);
-				}
-			}
-			for (const auto& i : remove_tmp) {
-				result.erase(
-					std::find(result.begin(), result.end(), i)
-				);
-			}
-			std::sort(result.begin(), result.end(), [](const T& a, const T& b) -> bool {
-				return a.get_start() < b.get_start();
-			});
-			return interval_collection(result);
-		}
-
-		interval_collection find_overlaps(const T& i) {
-			return interval_collection(d_root.find_overlaps(i));
-		}
-	};
-
-	// class emit
-	template<typename CharType>
-	class emit: public interval {
-	public:
-		typedef std::basic_string<CharType>  string_type;
-		typedef std::basic_string<CharType>& string_ref_type;
-
-	private:
-		string_type d_keyword;
-		unsigned    d_index = 0;
-
-	public:
-		emit()
-			: interval(-1, -1)
-			, d_keyword() {}
-
-		emit(size_t start, size_t end, string_type keyword, unsigned index)
-			: interval(start, end)
-			, d_keyword(keyword), d_index(index) {}
-
-		string_type get_keyword() const { return string_type(d_keyword); }
-		unsigned get_index() const { return d_index; }
-		bool is_empty() const { return (get_start() == -1 && get_end() == -1); }
-	};
-
-	// class token
-	template<typename CharType>
-	class token {
-	public:
-		enum token_type{
-			TYPE_FRAGMENT,
-			TYPE_MATCH,
-		};
-
-		using string_type     = std::basic_string<CharType>;
-		using string_ref_type = std::basic_string<CharType>&;
-		using emit_type       = emit<CharType>;
-
-	private:
-		token_type  d_type;
-		string_type d_fragment;
-		emit_type   d_emit;
-
-	public:
-		explicit token(string_ref_type fragment)
-			: d_type(TYPE_FRAGMENT)
-			, d_fragment(fragment)
-			, d_emit() {}
-
-		token(string_ref_type fragment, const emit_type& e)
-			: d_type(TYPE_MATCH)
-			, d_fragment(fragment)
-			, d_emit(e) {}
-
-		bool is_match() const { return (d_type == TYPE_MATCH); }
-		string_type get_fragment() const { return string_type(d_fragment); }
-		emit_type get_emit() const { return d_emit; }
-	};
-
 	// class state
 	template<typename CharType>
 	class state {
@@ -297,8 +44,8 @@ namespace aho_corasick {
 		typedef std::unique_ptr<state<CharType>> unique_ptr;
 		typedef std::basic_string<CharType>      string_type;
 		typedef std::basic_string<CharType>&     string_ref_type;
-		typedef std::pair<string_type, unsigned> key_index;
-		typedef std::set<key_index>              string_collection;
+		typedef std::pair<unsigned, unsigned> 	 key_index;
+		typedef std::list<key_index>             index_collection;
 		typedef std::vector<ptr>                 state_collection;
 		typedef std::vector<CharType>            transition_collection;
 
@@ -307,7 +54,8 @@ namespace aho_corasick {
 		ptr                            d_root;
 		std::map<CharType, unique_ptr> d_success;
 		ptr                            d_failure;
-		string_collection              d_emits;
+		bool						   d_has_keyword;
+		index_collection               d_emits;
 
 	public:
 		state(): state(0) {}
@@ -317,6 +65,7 @@ namespace aho_corasick {
 			, d_root(depth == 0 ? this : nullptr)
 			, d_success()
 			, d_failure(nullptr)
+			, d_has_keyword(false)
 			, d_emits() {}
 
 		ptr next_state(CharType character) const {
@@ -336,20 +85,23 @@ namespace aho_corasick {
 			return next;
 		}
 
+		void set_has_keyword() { d_has_keyword = true; }
+
+		bool has_keyword() { return d_has_keyword; }
+
 		size_t get_depth() const { return d_depth; }
 
-		void add_emit(string_ref_type keyword, unsigned index) {
-			d_emits.insert(std::make_pair(keyword, index));
+		void add_emit(unsigned keyword_index, unsigned position) {
+			d_emits.push_back(std::make_pair(keyword_index, position));
 		}
 
-		void add_emit(const string_collection& emits) {
+		void add_emit(const index_collection& emits) {
 			for (const auto& e : emits) {
-				string_type str(e.first);
-				add_emit(str, e.second);
+				add_emit(e.first, e.second);
 			}
 		}
 
-		string_collection get_emits() const { return d_emits; }
+		index_collection get_emits() const { return d_emits; }
 
 		ptr failure() const { return d_failure; }
 
@@ -389,30 +141,18 @@ namespace aho_corasick {
 	public:
 		using string_type = std::basic_string < CharType > ;
 		using string_ref_type = std::basic_string<CharType>&;
+		typedef enum match_type{ FULL, PREFIX, SUFFIX } match_type;
 
-		typedef state<CharType>         state_type;
-		typedef state<CharType>*        state_ptr_type;
-		typedef token<CharType>         token_type;
-		typedef emit<CharType>          emit_type;
-		typedef std::vector<token_type> token_collection;
-		typedef std::vector<emit_type>  emit_collection;
+		typedef state<CharType>         											   state_type;
+		typedef state<CharType>*        											   state_ptr_type;
+		typedef std::unordered_map<unsigned, std::unordered_map<match_type, unsigned>> emit_collection;
 
 		class config {
-			bool d_allow_overlaps;
-			bool d_only_whole_words;
 			bool d_case_insensitive;
 
 		public:
 			config()
-				: d_allow_overlaps(true)
-				, d_only_whole_words(false)
-				, d_case_insensitive(false) {}
-
-			bool is_allow_overlaps() const { return d_allow_overlaps; }
-			void set_allow_overlaps(bool val) { d_allow_overlaps = val; }
-
-			bool is_only_whole_words() const { return d_only_whole_words; }
-			void set_only_whole_words(bool val) { d_only_whole_words = val; }
+				: d_case_insensitive(false) {}
 
 			bool is_case_insensitive() const { return d_case_insensitive; }
 			void set_case_insensitive(bool val) { d_case_insensitive = val; }
@@ -437,110 +177,59 @@ namespace aho_corasick {
 			return (*this);
 		}
 
-		basic_trie& remove_overlaps() {
-			d_config.set_allow_overlaps(false);
-			return (*this);
-		}
-
-		basic_trie& only_whole_words() {
-			d_config.set_only_whole_words(true);
-			return (*this);
-		}
-
-		void insert(string_type keyword) {
+		void insert(string_type keyword, unsigned keyword_index, bool reverse) {
 			if (keyword.empty())
 				return;
 			state_ptr_type cur_state = d_root.get();
+			unsigned length = 0;
 			for (const auto& ch : keyword) {
+				length++;
 				cur_state = cur_state->add_state(ch);
+				cur_state->add_emit(keyword_index, length);
 			}
-			cur_state->add_emit(keyword, d_num_keywords++);
+			if (!reverse)
+			    cur_state->set_has_keyword();
 			d_constructed_failure_states = false;
 		}
 
-		template<class InputIterator>
-		void insert(InputIterator first, InputIterator last) {
-			for (InputIterator it = first; first != last; ++it) {
-				insert(*it);
-			}
-		}
-
-		token_collection tokenise(string_type text) {
-			token_collection tokens;
-			auto collected_emits = parse_text(text);
-			size_t last_pos = -1;
-			for (const auto& e : collected_emits) {
-				if (e.get_start() - last_pos > 1) {
-					tokens.push_back(create_fragment(e, text, last_pos));
-				}
-				tokens.push_back(create_match(e, text));
-				last_pos = e.get_end();
-			}
-			if (text.size() - last_pos > 1) {
-				tokens.push_back(create_fragment(typename token_type::emit_type(), text, last_pos));
-			}
-			return token_collection(tokens);
-		}
-
-		emit_collection parse_text(string_type text) {
+		emit_collection parse_text(string_type text, bool stop_at_full, emit_collection& collected_emits) {
 			check_construct_failure_states();
 			size_t pos = 0;
 			state_ptr_type cur_state = d_root.get();
-			emit_collection collected_emits;
 			for (auto c : text) {
 				if (d_config.is_case_insensitive()) {
 					c = std::tolower(c);
 				}
 				cur_state = get_state(cur_state, c);
-				store_emits(pos, cur_state, collected_emits);
 				pos++;
+				if (cur_state->has_keyword()) {
+				    store_emits(pos, cur_state, collected_emits, stop_at_full, FULL);
+				}
+				else if (pos == text.size()) {
+				    store_emits(pos, cur_state, collected_emits, stop_at_full, PREFIX);
+				}
 			}
-			if (d_config.is_only_whole_words()) {
-				remove_partial_matches(text, collected_emits);
-			}
-			if (!d_config.is_allow_overlaps()) {
-				interval_tree<emit_type> tree(typename interval_tree<emit_type>::interval_collection(collected_emits.begin(), collected_emits.end()));
-				auto tmp = tree.remove_overlaps(collected_emits);
-				collected_emits.swap(tmp);
+			return emit_collection(collected_emits);
+		}
+
+		emit_collection parse_reverse_text(string_type text, bool stop_at_full, emit_collection& collected_emits) {
+			check_construct_failure_states();
+			size_t pos = text.size()-1;
+			state_ptr_type cur_state = d_root.get();
+			for (auto rit=text.rbegin(); rit!=text.rend(); ++rit) {
+				if (d_config.is_case_insensitive()) {
+					*rit = std::tolower(*rit);
+				}
+				cur_state = get_state(cur_state, *rit);
+				pos--;
+				if (pos == -1) {
+				    store_emits(pos, cur_state, collected_emits, stop_at_full, SUFFIX);
+				}
 			}
 			return emit_collection(collected_emits);
 		}
 
 	private:
-		token_type create_fragment(const typename token_type::emit_type& e, string_ref_type text, size_t last_pos) const {
-			auto start = last_pos + 1;
-			auto end = (e.is_empty()) ? text.size() : e.get_start();
-			auto len = end - start;
-			typename token_type::string_type str(text.substr(start, len));
-			return token_type(str);
-		}
-
-		token_type create_match(const typename token_type::emit_type& e, string_ref_type text) const {
-			auto start = e.get_start();
-			auto end = e.get_end() + 1;
-			auto len = end - start;
-			typename token_type::string_type str(text.substr(start, len));
-			return token_type(str, e);
-		}
-
-		void remove_partial_matches(string_ref_type search_text, emit_collection& collected_emits) const {
-			size_t size = search_text.size();
-			emit_collection remove_emits;
-			for (const auto& e : collected_emits) {
-				if ((e.get_start() == 0 || !std::isalpha(search_text.at(e.get_start() - 1))) &&
-					(e.get_end() + 1 == size || !std::isalpha(search_text.at(e.get_end() + 1)))
-					) {
-					continue;
-				}
-				remove_emits.push_back(e);
-			}
-			for (auto& e : remove_emits) {
-				collected_emits.erase(
-					std::find(collected_emits.begin(), collected_emits.end(), e)
-					);
-			}
-		}
-
 		state_ptr_type get_state(state_ptr_type cur_state, CharType c) const {
 			state_ptr_type result = cur_state->next_state(c);
 			while (result == nullptr) {
@@ -582,12 +271,13 @@ namespace aho_corasick {
 			}
 		}
 
-		void store_emits(size_t pos, state_ptr_type cur_state, emit_collection& collected_emits) const {
+		void store_emits(size_t pos, state_ptr_type cur_state, emit_collection& collected_emits, bool stop_at_full, match_type mtype) const {
 			auto emits = cur_state->get_emits();
 			if (!emits.empty()) {
-				for (const auto& str : emits) {
-					auto emit_str = typename emit_type::string_type(str.first);
-					collected_emits.push_back(emit_type(pos - emit_str.size() + 1, pos, emit_str, str.second));
+				for (const auto& emit : emits) {
+					if (collected_emits[emit.first].find(FULL) == collected_emits[emit.first].end() || (mtype != FULL && !stop_at_full)) {
+				        collected_emits[emit.first].insert(std::make_pair(mtype, emit.second));
+					}
 				}
 			}
 		}
@@ -600,3 +290,5 @@ namespace aho_corasick {
 } // namespace aho_corasick
 
 #endif // AHO_CORASICK_HPP
+
+
